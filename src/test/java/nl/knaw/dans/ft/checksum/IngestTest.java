2199aaeb1062c07168bce4dc1d5fc0e658f67c89
henk van den berg <ecco@Hor-Vastus.local>
2015-04-22 17:56:38 +0200

    validationTest

--- src/test/java/nl/knaw/dans/ft/checksum/IngestTest.java
+++ src/test/java/nl/knaw/dans/ft/checksum/IngestTest.java
@@ -3,29 +3,96 @@ package nl.knaw.dans.ft.checksum;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.FileInputStream;
 
 import nl.knaw.dans.ft.AbstractFedoraTest;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
 import com.yourmediashelf.fedora.client.request.AddDatastream;
 import com.yourmediashelf.fedora.client.request.FindObjects;
+import com.yourmediashelf.fedora.client.request.GetDatastream;
+import com.yourmediashelf.fedora.client.request.GetDatastreamDissemination;
 import com.yourmediashelf.fedora.client.request.Ingest;
 import com.yourmediashelf.fedora.client.request.PurgeObject;
 import com.yourmediashelf.fedora.client.response.AddDatastreamResponse;
 import com.yourmediashelf.fedora.client.response.FedoraResponse;
 import com.yourmediashelf.fedora.client.response.FindObjectsResponse;
+import com.yourmediashelf.fedora.client.response.GetDatastreamResponse;
 import com.yourmediashelf.fedora.client.response.IngestResponse;
 
 public class IngestTest extends AbstractFedoraTest
 {
+    // Generate test files first with scripts/generate_files.sh
+    // This will create 28files, total 1Gb
     
     @Test
+    public void checksumValidTest() throws Exception {
+        //purgeExistingDOBs();
+        //ingestDOBs();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(verifyChecksums());
+        }
+    }
+    
+    private long verifyChecksums() throws Exception {
+        long time = 0L;
+        File[] testFiles = new File("test-files").listFiles();
+        for (File file : testFiles) {
+            String pid = CHECKSUM_PREFIX + ":" + file.getName();
+            //
+            long start = System.currentTimeMillis();
+            GetDatastreamResponse response = new GetDatastream(pid, "test")
+                    .validateChecksum(true)
+                    .execute();
+            
+            assertTrue(response.isChecksumValid());           
+            long duration = System.currentTimeMillis() - start;
+            //
+            time += duration;
+        }
+        return time;
+    }
+    
+    //GetDatastreamDissemination for files ~ 27000-30000
+    // so most ingest time is network overhead.
+    //@Test
+    public void verifyChecksumsLocal() throws Exception {
+        long time = 0L;
+        File[] testFiles = new File("test-files").listFiles();
+        for (File file : testFiles) {
+            String pid = CHECKSUM_PREFIX + ":" + file.getName();
+            
+            FileInputStream fis = new FileInputStream(file);
+            String checksum1 = DigestUtils.sha1Hex(fis);
+            fis.close();
+            //
+            long start = System.currentTimeMillis();
+            FedoraResponse response = new GetDatastreamDissemination(pid, "test")
+                    .execute();
+            String checksum2 = DigestUtils.sha1Hex(response.getEntityInputStream());
+            response.close();
+            assertEquals(checksum1, checksum2);
+            long duration = System.currentTimeMillis() - start;
+            time += duration;
+        }
+        System.out.println("Total validationt time: " + time);
+    }
+    
+    //@Test
+    public void tenIngestsTests() throws Exception {
+        for (int i = 1; i <= 10; i++) {
+            ingestTest();
+        }
+    }
+    
+    //@Test
     public void ingestTest() throws Exception {
         purgeExistingDOBs();
         calculateTestFilesLength();
-        ingestDOBs();
+        System.out.println(ingestDOBs());
     }
     
     private long ingestDOBs() throws Exception {
@@ -34,6 +101,10 @@ public class IngestTest extends AbstractFedoraTest
         // create objects
         for (File file : testFiles) {
             String pid = CHECKSUM_PREFIX + ":" + file.getName();
+            FileInputStream fis = new FileInputStream(file);
+            String checksum = DigestUtils.sha1Hex(fis);
+            fis.close();
+
             //
             long start = System.currentTimeMillis();
             IngestResponse ingResponse = new Ingest(pid)
@@ -42,6 +113,7 @@ public class IngestTest extends AbstractFedoraTest
             AddDatastreamResponse adsResponse = new AddDatastream(pid, "test")
                     .controlGroup("M")
                     .checksumType("SHA-1")
+                    .checksum(checksum) // if not valid: org.fcrepo.server.errors.ValidationException: Checksum Mismatch
                     .mimeType("application/octet-stream")
                     .content(file)
                     .execute();
@@ -49,13 +121,12 @@ public class IngestTest extends AbstractFedoraTest
             //
             assertEquals(201, ingResponse.getStatus());
             assertEquals(201, adsResponse.getStatus());
-            //assertFalse(adsResponse.isChecksumValid());
-            //assertTrue(adsResponse.isChecksumValid());
+            assertFalse(adsResponse.isChecksumValid()); // always false
             time += duration;
             
             //System.out.println("Ingest: size=" + file.length()/1000000L + " duration=" + duration);
         }
-        System.out.println("Total ingest time: " + time);
+        //System.out.println("Total ingest time: " + time);
         return time;
     }
     
@@ -65,7 +136,7 @@ public class IngestTest extends AbstractFedoraTest
         for (File file : testFiles) {
             size += file.length();
         }
-        System.out.println(testFiles.length + " test files: " + FileUtils.byteCountToDisplaySize(size));
+        //System.out.println(testFiles.length + " test files: " + FileUtils.byteCountToDisplaySize(size));
         return size;
     }
     
@@ -80,7 +151,7 @@ public class IngestTest extends AbstractFedoraTest
             FedoraResponse pr = new PurgeObject(pid).execute();
             assertEquals(200, pr.getStatus());
         }
-        System.out.println("Purged " + response.getPids().size() + " '" + CHECKSUM_PREFIX + "' test objects.");
+        //System.out.println("Purged " + response.getPids().size() + " '" + CHECKSUM_PREFIX + "' test objects.");
     }
     
 
